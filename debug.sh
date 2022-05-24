749cd07b910d95a19f7554463c3ec5bbaedfb5c1
amungun <amungun@gmail.com>
2022-05-24 18:10:36 +0000

    committing new changes

--- debug.sh
+++ debug.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "I am rich"
-echo 'DevOps is easy'
-echo "today is the: 'date'"
-Date
-echo "this is a modification
+echo "DevOps is easy"
+echo "today is the:"
+date
+echo "this is a modification"
+sleep 5
 echo "this modification is complete"
 echo "error corrected"
